a31d5c749de85343a60054a751c5a5c340ce6813
Anthony Mai <Mai_Anthony@hotmail.com>
2019-03-25 05:51:59 +0000

    Make it build on an Arm64 System
    Tweak the cmake file to make it build on an ARM64 system, a Nano Pi.

---
+++ src/arm64/clock.s
@@ -0,0 +1,17 @@
+.text	
+
+.globl  rd_clk
+.type   rd_clk,@function
+.align  16
+rd_clk:
+    ret
+.size   rd_clk,.-rd_clk
+
+.globl	rd_clk2
+.type	rd_clk2,@function
+.align	16
+rd_clk2:
+    ret
+.size	rd_clk2,.-rd_clk2
+
+
