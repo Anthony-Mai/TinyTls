2b960044b5640410eb32a0beafdab2e6b705b7c4
Anthony Mai <Mai_Anthony@hotmail.com>
2019-04-14 03:19:11 -0700

    Add 64x64 Bits Polynomial Multiplication Using Linux x86 Assembly Code
    Polynomial multiplication, also known as carriless multiplication, is
    very important in many cryptography applications including AES-GCM.
    
    This implements the 64x64 bits polynomial multiplication results in
    128 bits result. This code is for Linux x86 (32 bits) build only.
    
    This may not work if the CPU does not have support of the pclmulqdq
    instruction. Check by cat /proc/cpuinfo and look for it in flags.

--- src/i386/clock.s
+++ src/i386/clock.s
@@ -18,4 +18,20 @@ rd_clk2:
     ret
 .size	rd_clk2,.-rd_clk2
 
+// Polynomial product of 64 bits x 64 bits produce 128 bits.
+// Usage: uint64_t PMull8x8(uint64_t a, uint64_t b, uint64_t r[2]);
+.globl  PMull8x8
+.type   PMull8x8,@function
+.align  16
+PMull8x8:
+    movups 0x04(%esp), %xmm0
+    pclmulqdq $1, %xmm0, %xmm0
+    mov  0x14(%esp), %edx
+    movd %xmm0, %eax
+    movups %xmm0, (%edx)
+    psrldq $4, %xmm0
+    movd %xmm0, %edx
+    emms
+    ret
+.size   PMull8x8,.-PMull8x8
 
